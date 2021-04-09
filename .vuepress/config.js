13fc3de94f00ae770fe7c81bdadca2bfc25c6fa9
songmingyue <1577323783@qq.com>
2021-04-09 16:44:52 +0800

    fix: add

--- .vuepress/config.js
+++ .vuepress/config.js
@@ -1,6 +1,6 @@
 module.exports = {
-  "title": "",
-  "description": "",
+  "title": "宋宋的周报",
+  "description": "我盼世界无 bug",
   "dest": "public",
   "head": [
     [
@@ -16,10 +16,12 @@ module.exports = {
         "name": "viewport",
         "content": "width=device-width,initial-scale=1,user-scalable=no"
       }
-    ]
+    ],
+    
   ],
   "theme": "reco",
   "themeConfig": {
+    author: '码农老伙计',
     "nav": [
       {
         "text": "周报",
@@ -72,20 +74,20 @@ module.exports = {
         "text": "Tag"
       }
     },
-    "friendLink": [
-      {
-        "title": "午后南杂",
-        "desc": "Enjoy when you can, and endure when you must.",
-        "email": "1156743527@qq.com",
-        "link": "https://www.recoluan.com"
-      },
-      {
-        "title": "vuepress-theme-reco",
-        "desc": "A simple and beautiful vuepress Blog & Doc theme.",
-        "avatar": "https://vuepress-theme-reco.recoluan.com/icon_vuepress_reco.png",
-        "link": "https://vuepress-theme-reco.recoluan.com"
-      }
-    ],
+    // "friendLink": [
+    //   {
+    //     "title": "午后南杂",
+    //     "desc": "愿世界没有bug",
+    //     "email": "1156743527@qq.com",
+    //     "link": "https://www.recoluan.com"
+    //   },
+    //   {
+    //     "title": "vuepress-theme-reco",
+    //     "desc": "A simple and beautiful vuepress Blog & Doc theme.",
+    //     "avatar": "https://vuepress-theme-reco.recoluan.com/icon_vuepress_reco.png",
+    //     "link": "https://vuepress-theme-reco.recoluan.com"
+    //   }
+    // ],
     "logo": "/logo.png",
     "search": false,
     "searchMaxSuggestions": 10,
@@ -93,8 +95,59 @@ module.exports = {
     "author": "",
     "authorAvatar": "/avatar.png",
     "record": "xxxx",
-    "startYear": "2017"
+    "startYear": "2021"
   },
+  plugins: [
+    [
+      "@vuepress-reco/vuepress-plugin-kan-ban-niang",
+      {
+        theme: ['koharu', 'blackCat', 'whiteCat', 'haru1', 'haru2', 'haruto', 'izumi', 'shizuku', 'wanko', 'miku', 'z16'],
+        clean: false,
+        messages: {
+          welcome: '马什么梅',
+          home: '马东什么',
+          theme: '什么冬梅',
+          close: '别不要我嘛'
+        },
+        width:140,
+        height:252
+      }
+    ],
+    [
+      "cursor-effects",
+      {
+        size: 3,                    // size of the particle, default: 2
+        shape: ['circle'],  // shape of the particle, default: 'star'
+        zIndex: 999999999           // z-index property of the canvas, default: 999999999
+      }
+    ],
+    [
+      "dynamic-title",
+      {
+        showIcon: "/favicon.ico",
+        showText: "(/≧▽≦/)咦！又好了！",
+        hideIcon: "/failure.ico",
+        hideText: "(●—●)喔哟，崩溃啦！",
+        recoverTime: 2000
+      }
+    ],
+    [
+      //图片放大插件 先安装在配置， npm install vuepress-plugin-dynamic-title --save
+      '@vuepress/plugin-medium-zoom', {
+        selector: '.page img',
+        delay: 1000,
+        options: {
+          margin: 24,
+          background: 'rgba(25,18,25,0.9)',
+          scrollOffset: 40
+        }
+      }
+    ],
+    [
+      //插件广场的流程图插件 先安装在配置 npm install vuepress-plugin-flowchart --save
+      'flowchart'
+    ],
+  ],
   "markdown": {
     "lineNumbers": true
   }
